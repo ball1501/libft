9b8b8059dc5c60d4b9552ef6225aed0b3fdaa276
ball1501 <ball_zider@hotmail.com>
2026-08-26 08:59:15 +0700

    add ft_strlen = src_len

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,9 +6,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	src_len;
 
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
+	src_len = ft_strlen(src);
 	dst_len = 0;
 	while (dst_len < dstsize && dst[dst_len])
 		dst_len++;
