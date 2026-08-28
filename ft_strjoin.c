5eb76d271e62a04d289d01110662ac6e88ce0ef4
ball1501 <ball_zider@hotmail.com>
2026-08-28 10:41:40 +0700

    change malloc to collac

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,19 +2,18 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*str;
 	size_t	len1;
 	size_t	len2;
-	char	*str;
 
 	if (!s1 || !s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	str = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(str + len1, s2, len2);
-	str[len1 + len2] = '\0';
 	return (str);
 }
