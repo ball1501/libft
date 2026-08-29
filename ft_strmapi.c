bf90840bb4cf4f53c1f650862bfc161ff4ac9d09
ball1501 <ball_zider@hotmail.com>
2026-08-29 10:18:46 +0700

    change malloc to calloc

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -2,14 +2,12 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	len;
 	size_t	i;
 	char	*ptr;
 
 	if (!s || !f)
 		return (NULL);
-	len = ft_strlen(s);
-	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	ptr = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(char));
 	if (!ptr)
 		return (NULL);
 	i = 0;
@@ -18,6 +16,5 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		ptr[i] = f(i, s[i]);
 		i++;
 	}
-	ptr[i] = '\0';
 	return (ptr);
 }
