4a6f7bd26fc043f18b9960b0deca4819badbe008
ball1501 <ball_zider@hotmail.com>
2026-08-29 18:48:23 +0700

    change to __INT_MAX__ in condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wngamkri <wngamkri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/29 16:27:32 by wngamkri          #+#    #+#             */
-/*   Updated: 2026/08/29 16:27:33 by wngamkri         ###   ########.fr       */
+/*   Updated: 2026/08/29 18:42:33 by wngamkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
-	size_t	total_size;
+	void	*res;
 
-	total_size = count * size;
-	if (count != 0 && total_size / count != size)
+	if (count == 0 || size == 0)
+		return (malloc(1));
+	if ((count * size) > __INT_MAX__
+		|| size > __INT_MAX__
+		|| count > __INT_MAX__)
 		return (NULL);
-	ptr = malloc(total_size);
-	if (!ptr)
+	res = malloc(count * size);
+	if (!res)
 		return (NULL);
-	ft_bzero(ptr, total_size);
-	return (ptr);
+	ft_bzero(res, count * size);
+	return (res);
 }
