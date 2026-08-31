3d436f2024a581c6245c7cf5b11b608e607f642e
ball1501 <ball_zider@hotmail.com>
2026-08-31 10:03:42 +0700

    delete comment

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: wngamkri <wngamkri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/29 16:31:07 by wngamkri          #+#    #+#             */
-/*   Updated: 2026/08/29 16:31:08 by wngamkri         ###   ########.fr       */
+/*   Updated: 2026/08/31 10:03:24 by wngamkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	// เช็กกรณี c เป็น '\0' (หรือ 0) ที่ตัวปิดท้าย String
 	if ((unsigned char)s[i] == (unsigned char)c)
 		return ((char *)&s[i]);
 	return (NULL);
